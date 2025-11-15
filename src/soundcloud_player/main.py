a656de9b5e4e32611cd3306090fdfadc3aad905d
Alexander Bader <lxbader@gmail.com>
2025-11-15 12:28:52 +0100

    feat: allow resetting config; make track length limit a CLI arg

--- src/soundcloud_player/main.py
+++ src/soundcloud_player/main.py
@@ -1,10 +1,6 @@
 import argparse
-import os
-import sys
-from pathlib import Path
 
-import yaml
-
+from soundcloud_player.config_manager import ConfigManager
 from soundcloud_player.player import Player
 from soundcloud_player.soundcloud_client import SoundCloudClient
 
@@ -13,54 +9,26 @@ def create_parser():
     parser = argparse.ArgumentParser(
         description="Stream music from your SoundCloud feed or likes"
     )
-    # parser.add_argument(
-    #     "username", help="SoundCloud username whose liked tracks to play"
-    # )
+    parser.add_argument(
+        "--min-track-length",
+        "-m",
+        help="Minimum track length to filter feed on [minutes]",
+        default=30,
+        type=int,
+    )
+    parser.add_argument(
+        "--reset-config", "-r", help="Reset config", action="store_true"
+    )
     return parser
-
-
-class ConfigManager:
-    def __init__(self):
-        if sys.platform == "darwin":
-            data_root = Path.home() / "Library"
-        elif sys.platform == "win32":
-            data_root = os.getenv("LOCALAPPDATA")
-        else:
-            raise NotImplementedError("Only Windows and MacOS are supported")
-        data_dir = Path(data_root) / "scplay"
-        data_dir.mkdir(exist_ok=True, parents=True)
-        self.cfg_file = data_dir / "config.yaml"
-        if not self.cfg_file.exists():
-            self.create()
-        self.settings = self.load()
-        self.maybe_add_oauth_token()
-        self.write()
-
-    def create(self):
-        print(f"Creating config file at '{self.cfg_file}'...")
-        self.cfg_file.touch()
-
-    def load(self) -> dict:
-        with open(self.cfg_file, "r") as f:
-            content = yaml.load(f, yaml.SafeLoader)
-        return content or {}
-
-    def maybe_add_oauth_token(self) -> None:
-        if "oauth-token" not in self.settings:
-            self.settings["oauth-token"] = input(
-                "Please enter your SoundCloud oauth token: "
-            )
-
-    def write(self):
-        with open(self.cfg_file, "w") as f:
-            yaml.dump(self.settings, f)
 
 
 def main():
     args = create_parser().parse_args()
     cfg = ConfigManager()
+    if args.reset_config:
+        cfg.set_oauth_token()
     client = SoundCloudClient(cfg.settings["oauth-token"])
-    app = Player(sc_client=client)
+    app = Player(sc_client=client, min_track_length_sec=args.min_track_length * 60)
     app.run()
 
 
